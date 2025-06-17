0b967e5f5e67c88d365cc73f70f456323b21fe36
Vadim Bezborodov <vadiousyande@yandex.ru>
2025-06-17 17:56:18 +0300

    second commit

--- src/main/java/com/stepup/proj/Main.java
+++ src/main/java/com/stepup/proj/Main.java
@@ -3,5 +3,8 @@ package com.stepup.proj;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
